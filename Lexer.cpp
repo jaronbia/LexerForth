dd8e7d48c052c058b5a92f2929105e82f57f8343
Jaron Bialecki <jaronb@Jarons-MacBook-Pro.local>
2020-03-02 15:46:49 -0500

    Reads file line by line using getline and checks for eof.

--- Lexer.cpp
+++ Lexer.cpp
@@ -1,29 +1,54 @@
+/***************************************
+ * Author: Jaron Bialecki & Evan Perry
+ * Date: March 2nd, 2020
+***************************************/
+
 #include "Lexer.hpp"
 
-Lexer::Lexer(string filename) {
+Lexer::
+Lexer(string filename) {
     in.open(filename);
+    lex();
 }
 
+//-------------------------------------------------------------------------
 void Lexer::
 lex() {
+    string line;
 
+    for(int j = 0; ; ++j) {
+        getline(in, line);
+        
+        if(line[j] == ' ') continue;
+        if(in.eof() || !in.good()) break;
+
+        //for(int k = j + 1; ; ++k) {
+
+        //}
+
+        cout << line << "\n";
+    }
 }
 
+//-------------------------------------------------------------------------
 void Lexer::
-startLex() {
-
+startLex() { 
+    
 }
 
-void Lexer::
+//-------------------------------------------------------------------------
+void Lexer:: 
 acquireSymbol() {
 
 }
 
+//-------------------------------------------------------------------------      
 void Lexer::
 acquireString() {
 
 }
 
+//-------------------------------------------------------------------------
 void Lexer::
 foundToken() {
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
+/***************************************
+ * Author: Jaron Bialecki & Evan Perry
+ * Date: March 2nd, 2020
+***************************************/
 
+#include <iostream>
 #include "Lexer.hpp"
 
-int main()
-{
-    cout << "Compiles successfully";
+int main(int argc, char** argv) {
+    string filename(argv[1]);
+    Lexer ForthLex(filename);
+
     return 0;
 }
