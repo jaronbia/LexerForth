b4bdabaf0023e0b9b2d265ff1f46e1602b3c9877
Evan Perry <deepthought39@yahoo.com>
2020-03-02 23:32:27 -0500

    fixed error found, commented out for now. Tested with reading in input from line, for now it can print it to the screen.

--- Lexer.cpp
+++ Lexer.cpp
@@ -7,8 +7,15 @@
 
 Lexer::
 Lexer(string filename) {
+    cout << filename << endl;
     in.open(filename);
-    lex();
+    if (in.good()) {
+        lex();
+    }
+    else {
+        cout << "Error, unable to open file." << endl;
+    }
+    
 }
 
 //-------------------------------------------------------------------------
@@ -16,11 +23,10 @@ void Lexer::
 lex() {
     string line;
 
-    for(int j = 0; ; ++j) {
+    for(int j = 0; ; j++) {
         getline(in, line);
-        
-        if(line[j] == ' ') continue;
-        if(in.eof() || !in.good()) break;
+        //if (line[j] == ' ')continue;  <-This line causes an error when you read in, commented out
+        if (in.eof() || !in.good()) break;
 
         //for(int k = j + 1; ; ++k) {
 
@@ -28,6 +34,7 @@ lex() {
 
         cout << line << "\n";
     }
+    in.close();
 }
 
 //-------------------------------------------------------------------------
