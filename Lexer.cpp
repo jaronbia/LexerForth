d531beb85fb992c3118248b3226465c67992da12
Evan Perry <deepthought39@yahoo.com>
2020-03-14 15:31:10 -0400

    modified code to show we can pass control from startLex to acrquire symbol and we delegate to acquiresymbol to print.

--- Lexer.cpp
+++ Lexer.cpp
@@ -14,12 +14,13 @@ Lexer::Lexer(string filename) {
 void Lexer::lex() {
     State st = START;
     string line;
+    int x = 1;
 
     for (;;) {
         getline(in, line);
         if (in.eof() || !in.good()) break;
         if (st == START) startLex(line, st);
-        else if (st == SYMBOL) acquireSymbol(line, st);
+        if (st == SYMBOL) acquireSymbol(line, st);
         else if (st == STRINGS) acquireString(line, st);
         else foundToken(line, st);
     }
@@ -28,50 +29,56 @@ void Lexer::lex() {
 void Lexer::startLex(string& line, State& st) {
     int j, k = 1;
     bool skipws = false;
+    bool fSymbol = false;
     Ttype = NUMBER;
     if (endParen) {
         temp.clear();
     }
-    if (line.empty()) { // empty line, return to lex()
-        return;
-    }
-    else {
-        //cout << line.length();
+    if (!line.empty()) {
         for (j = 0; j < line.length(); j++) {   // clear whitespace
             if (!isspace(line[j]) && !skipws && line[j] != 92 && endParen && line[j] != 40) {
                 temp += line[j];
                 skipws = true;
+                fSymbol = true;
             }
             else if (skipws && line[j] != 92 && endParen && line[j] != 40) {
                 temp += line[j];
+                if (!fSymbol) {
+                    fSymbol = true;
+                }
             }
             else if (line[j] == 92 && isspace(line[k]) && endParen) {
-                temp = line.substr(j);
-                Ttype = COMMENT;
+                cout << line.substr(j) << endl;
                 break;
             }
             else if (line[j] == 40 || !endParen) {
+
                 if (line[j] == 41) {
+                    cout << endl;
                     endParen = true;
-                    Ttype = COMMENTBLK;
                     break;
                 }
-                if (endParen) {
-                    print(cout);
-                    temp.clear();
-                }
+                cout << line[j];
                 endParen = false;
-                temp += line[j];
+                if (endParen) {
+                    break;
+                }
             }
             k++;
-
         }
     }
-    print(cout);
+    if (fSymbol && endParen) {
+        st = SYMBOL;
+        line = temp;
+
+    }
 }
 
 void Lexer::acquireSymbol(string& line, State& st) {
-
+    temp.clear();
+    temp = line;
+    print(cout);
+    st = START;
 }
 
 void Lexer::acquireString(string& line, State& st) {
@@ -83,16 +90,9 @@ void Lexer::foundToken(string& line, State& st) {
 }
 
 ostream& Lexer::print(ostream& out) {
-    if (Ttype == COMMENT) {
-        out << "One line comment found: " << temp << '\n';
-    }
-    else if (Ttype == COMMENTBLK) {
-        out << "Block Comment found: " << temp << '\n';
-    }
-    else {
+    if (!temp.empty()) {
         out << "not comment: ";
         out << temp << '\n';
     }
-
     return out;
 }
